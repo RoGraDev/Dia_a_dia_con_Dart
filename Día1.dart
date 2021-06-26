1d52124e8c0ac4c24a549318339ee5483af48396
RogerCode <70453253+RogerGrajalesG@users.noreply.github.com>
2021-06-26 09:12:21 -0500

    Update Día1.dart
    He intentado comentar el código.

--- Día1.dart
+++ Día1.dart
@@ -1,14 +1,23 @@
-/*Reto día 1 con Dart - Hola Mundo 
-En lugar de solo imprimir en pantalla, 
-pedirás al usuario que digite un nombre 
-y mostrarás en pantalla lo siguiente:
+/*Reto del día 1 con Dart:
+
+Hola Mundo 
+
+En lugar de solo imprimir 
+en pantalla, pedirás al 
+usuario que digite un nombre 
+y mostrarás en pantalla 
+lo siguiente:
+
 Hola, [nombre]*/
 
-import 'dart:io';
+import 'dart:io'; /*necesaria para leer de pantalla */
 
 void main() {
-  print('Ingrese su nombre: ');
-  var name = stdin.readLineSync();
+  print(
+      'Ingrese su nombre: '); /*Imprimimos en pantalla la petición de que ingrese su nombre*/
+  var name = stdin
+      .readLineSync(); /*Leemos lo que el usuario ingresó y lo almacenamos en la variable "name"*/
 
-  print("\nHola $name\n");
+  print(
+      "\nHola $name\n"); /*Imprimimos en pantalla un saludo haciendo uso del valor almacenado por la variable*/
 }
