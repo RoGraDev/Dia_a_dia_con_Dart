1ed1d7f7980afc045436366154e1c17687400a53
RogerCode <70453253+RogerGrajalesG@users.noreply.github.com>
2021-07-02 06:56:52 -0500

    Dia1 Folder
    Dia1 folder created

--- Dia1/Dia1.dart
+++ Dia1/Dia1.dart
@@ -1,15 +1,3 @@
-/*Reto del día 1 con Dart:
-
-Hola Mundo 
-
-En lugar de solo imprimir 
-en pantalla, pedirás al 
-usuario que digite un nombre 
-y mostrarás en pantalla 
-lo siguiente:
-
-Hola, [nombre]*/
-
 import 'dart:io'; /*necesaria para leer de pantalla */
 
 void main() {
---
+++ Dia1/Dia1.md
@@ -0,0 +1,11 @@
+/*Reto del día 1 con Dart:
+
+Hola Mundo 
+
+En lugar de solo imprimir 
+en pantalla, pedirás al 
+usuario que digite un nombre 
+y mostrarás en pantalla 
+lo siguiente:
+
+Hola, [nombre]*/
